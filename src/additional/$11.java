36dffd16bd46012bc04d007da4525a2760b2eea5
Skylore <iturchin98@gmail.com>
2016-10-10 22:04:22 +0300

    Update $11.java

--- src/additional/$11.java
+++ src/additional/$11.java
@@ -6,14 +6,16 @@ public class $11 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
-
-        if ((a % 10 == 1) || (a / 10 == 0.1))
+        int a1 = a % 10;
+        int a2 = a / 10;
+        
+        if (a1 == 1 || a2 == 0.1)
             System.out.println(a + " копейка");
-        else if ((a % 10 == 2) || (a / 10 == 0.2))
+        else if (a1 == 2 || a2 == 0.2)
             System.out.println(a + " копейки");
-        else if ((a % 10) == 3 || (a / 10 == 0.3))
+        else if (a1 == 3 || a2 == 0.3)
             System.out.println(a + " копейки");
-        else if ((a % 10) == 4 || (a / 10 == 0.4))
+        else if (a1 == 4 || a2 == 0.4)
             System.out.println(a + " копейки");
         else
             System.out.println(a + " копеек");
