3958e29905737e6c9ba2b25c7645831aac39492e
Skylore <iturchin98@gmail.com>
2016-10-10 21:05:21 +0300

    Update $4_4.java

--- src/$4_4.java
+++ src/$4_4.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 public class $4_4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your num");
         double a = scanner.nextDouble();
 
-        if (a >= 0 && a <= 1) System.out.println(true);
+        if (a >= 0 && a <= 1) System.out.println("0 < " + a  + " < 1");
         else System.out.println(false);
     }
 }
