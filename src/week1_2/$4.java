7388a9a63a9e79a8ca99cd5b0dfa4546d637a311
skylore <iturchin98@gmail.com>
2016-10-11 22:48:34 +0300

    .

---
+++ src/week1_2/$4.java
@@ -0,0 +1,16 @@
+package week1_2;
+
+import java.util.Scanner;
+
+public class $4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter num of digits in array");
+            int a = scanner.nextInt();
+            int array[] = new int[a];
+            int lenght = array.length;
+
+        System.out.println(lenght);
+    }
+}
