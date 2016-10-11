868c745f7c4c22e548f3c8992a8020c7153a3cb6
skylore <iturchin98@gmail.com>
2016-10-11 22:33:21 +0300

    .

---
+++ src/week1_2/$3.java
@@ -0,0 +1,13 @@
+package week1_2;
+
+public class $3 {
+    public static void main(String[] args) {
+        int mas[] = {23,45,-232,456,745};
+        int mas1[] = {-456,4576,-24,67,34};
+        int size = mas.length;
+        mas1 = mas.clone();
+
+        for (int i = 0; i < size; i++)
+            System.out.print(" " + mas1[i]);
+    }
+}
