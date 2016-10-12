6b465df4c5077dc6f111664ef111e40a44c01b5d
skylore <iturchin98@gmail.com>
2016-10-12 19:04:58 +0300

    .

---
+++ src/week1_2/$5.java
@@ -0,0 +1,35 @@
+package week1_2;
+
+import java.util.Scanner;
+
+public class $5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Set size of array ");
+        int size = scanner.nextInt();
+        System.out.println("Enter range of nums ");
+        int range = scanner.nextInt();
+
+        if (size < 0 || range <= 0) return;
+
+        int mas[] = new int[size];
+
+        for (int i = 1; i < size; i += 2) {
+            int random = (int) (range * Math.random());
+            if ((random % 2) == 0)
+                mas[i] = random;
+            else
+                mas[i] = random + 1;
+        }
+        for (int i = 0; i < size; i += 2) {
+            int random = (int) (range * Math.random());
+            if ((random % 2) == 1)
+                mas[i] = random;
+            else
+                mas[i] = random + 1;
+        }
+        for (int i = 0; i < size; i++)
+            System.out.print(mas[i] + " ");
+    }
+}
