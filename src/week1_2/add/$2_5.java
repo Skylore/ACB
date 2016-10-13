5e1c6fb9a58b0d9212023ccc1faf48dc70da97b1
skylore <iturchin98@gmail.com>
2016-10-13 19:05:46 +0300

    .

---
+++ src/week1_2/add/$2_5.java
@@ -0,0 +1,21 @@
+package week1_2.add;
+
+import java.util.Scanner;
+
+public class $2_5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your word ");
+        String line = scanner.next();
+
+        char[] mas = line.toCharArray();
+        int size = mas.length;
+
+        for (int i = 0; i < size; i++) {
+            if (mas[i] == 'a' || mas[i] == 'A')
+                mas[i] = '@';
+        }
+        String fin = new String(mas);
+        System.out.println(fin);
+    }
+}
