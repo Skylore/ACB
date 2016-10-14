4ed69709c6349c5191aca087dd33770f60dbd8af
skylore <iturchin98@gmail.com>
2016-10-14 17:53:14 +0300

    .

--- src/week1_2/$1.java
+++ src/week1_2/$1.java
@@ -2,18 +2,28 @@ package week1_2;
   //find min and max values of array and sout them
 public class $1 {
     public static void main(String[] args) {
-        int mas[] = {234,523,5235,53,-23,35,-3252,3256246,543252,233,4545,-234456};
-        int length = mas.length;
+        int mas[] = new int[10];
+        randomArray(mas);
 
-        for (int a = 1; a < length; a++)
-            for (int b = length - 1; b >= a; b--) {
+        for (int a = 1; a < mas.length; a++) {
+            for (int b = mas.length - 1; b >= a; b--) {
                 if (mas[b - 1] > mas[b]) {
                     int c = mas[b - 1];
                     mas[b - 1] = mas[b];
                     mas[b] = c;
                 }
             }
-        System.out.println("Min value is " + mas[0]);
-        System.out.println("Max value is " + mas[length - 1]);
+        }
+
+        System.out.println("Min value is " + mas[0]
+         + "\nMax value is " + mas[mas.length - 1]);
+        
+    }
+    public static int[] randomArray(int mas[]) {
+        for (int i = 0; i < mas.length; i++) {
+            int random = ((int) (100 * Math.random()));
+            mas[i] = random;
+        }
+        return mas;
     }
 }
