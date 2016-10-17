630f860ae22de6c5e1c1a71065261b3a2a97b13b
Skylore <iturchin98@gmail.com>
2016-10-18 00:45:46 +0300

    *

--- src/week1_2/add/$2_5.java
+++ src/week1_2/add/$2_5.java
@@ -5,8 +5,14 @@ import java.util.Scanner;
 public class $2_5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Enter your word ");
         String line = scanner.next();
+
+        System.out.println(changeA(line));
+    }
+
+    public static String changeA(String line) {
 
         char[] mas = line.toCharArray();
         int size = mas.length;
@@ -14,8 +20,11 @@ public class $2_5 {
         for (int i = 0; i < size; i++) {
             if (mas[i] == 'a' || mas[i] == 'A')
                 mas[i] = '@';
+
         }
+
         String fin = new String(mas);
-        System.out.println(fin);
+
+        return fin;
     }
 }
