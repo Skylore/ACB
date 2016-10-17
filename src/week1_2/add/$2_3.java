1c9d403cc265035cb273c52dc859b99d558999ae
Skylore <iturchin98@gmail.com>
2016-10-18 00:39:37 +0300

    *

--- src/week1_2/add/$2_3.java
+++ src/week1_2/add/$2_3.java
@@ -5,17 +5,25 @@ import java.util.Scanner;
 public class $2_3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Enter word ");
         String line = scanner.next();
 
+        System.out.println("Quantity of 'a' in this word is: " + count(line));
+    }
+
+    public static int count(String line) {
+
         char[] chMas = line.toCharArray();
         int size = chMas.length;
+
         int counter = 0;
 
         for (int i = 0; i < size; i++) {
             if (chMas[i] == 'a' || chMas[i] == 'A')
                 counter += 1;
         }
-        System.out.println("Quantity of 'a' in this word is: " + counter);
+
+        return counter;
     }
 }
