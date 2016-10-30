bdbc6f41ae760c73c0c9819bbe3877fb359cc5f2
Skylore <iturchin98@gmail.com>
2016-10-30 11:00:47 +0200

    .

--- src/week3/warships/GameLoop.java
+++ src/week3/warships/GameLoop.java
@@ -4,27 +4,17 @@ import week3.PrintMatrix;
 
 public class GameLoop {
 
-
-    private static int                     enemyX = ((int) (Math.random() * 10));
-
-    public static int getEnemyX() {
-        return enemyX;
-    }
-
-    private static int                     enemyY = ((int) (Math.random() * 10));
-
-    public static int getEnemyY() {
-        return enemyY;
-    }
-
     public static void run() {
 
-        int[][] mat = new int[10][10];
+        int enemyX = Steps.enemyX;
+        int enemyY = Steps.enemyY;
 
-        while (mat[enemyX][enemyY] == 0) {
+        int[][] mat = new int[10][10];
+        System.out.println(enemyX + " " + enemyY);
+
+        while (mat[enemyY][enemyX] == 0) {
             mat = Steps.step(mat);
             PrintMatrix.printMat(mat);
-            Hints.hint();
         }
     }
 }
--- src/week3/warships/Steps.java
+++ src/week3/warships/Steps.java
@@ -4,30 +4,56 @@ import java.util.Scanner;
 
 public class Steps {
 
-    public static int inputX() {
-        int                       x = new Scanner(System.in).nextInt();
 
-        return x;
-    }
+    public static int                     enemyX = ((int) (Math.random() * 10));
 
-    public static int inputY() {
-        int                       y = new Scanner(System.in).nextInt();
-
-        return y;
-    }
+    public static int                     enemyY = ((int) (Math.random() * 10));
 
     public static int[][] step(int[][] mat) {
+         
 
         System.out.println("Input your shoot: \nx - ");
-        int                                            x = inputX();
+        int                               inputX = new Scanner(System.in).nextInt();
         System.out.println("y - ");
-        int                                            y = inputY();
-
-        if (y <= 0 && y > 10 && x <= 0 && x > 10) {
+        int                               inputY = new Scanner(System.in).nextInt();
+        
+        
+        if (inputY <= 0 && inputY > 10 && inputX <= 0 && inputX > 10) {
             return mat;
         }
+        
 
-        mat[y][x] = 1;
+        mat[inputY][inputX] = 1;
+
+        //    Hints, TODO fix this
+
+        if (inputX == enemyX && inputY == enemyY) {
+            System.out.println("You win");
+
+        } else if (inputX < enemyX && inputY < enemyY) {
+            System.out.println("you need to shoot righter and lower");
+
+        } else if (inputX < enemyX && inputY > enemyY) {
+            System.out.println("you need to shoot righter and higher");
+
+        } else if (inputX > enemyX && inputY < enemyY) {
+            System.out.println("you need to shot lefter and lower");
+
+        } else if (inputX > enemyX && inputY > enemyY) {
+            System.out.println("you need to shoot lefter and higher");
+
+        } else if (inputX < enemyX && inputY == enemyY) {
+            System.out.println("you need to shoot righter");
+
+        } else if (inputX > enemyX && inputY == enemyY) {
+            System.out.println("you need to shoot lefter");
+
+        } else if (inputX == enemyX && inputY < enemyY) {
+            System.out.println("you need to shoot lower");
+
+        } else if (inputX == enemyX && inputY > enemyY) {
+            System.out.println("you need to shoot upper");
+        }
 
         return mat;
     }
